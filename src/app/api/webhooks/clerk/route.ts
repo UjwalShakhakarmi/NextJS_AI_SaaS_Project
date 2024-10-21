cc6b3549d94ad014969e505fe2eecfa393b1d566
UjwalShakhakarmi <ujwalshakha@gmail.com>
2024-10-21 22:52:24 +0545

    lastONe

--- src/app/api/webhooks/clerk/route.ts
+++ src/app/api/webhooks/clerk/route.ts
@@ -1,11 +1,14 @@
 /* eslint-disable camelcase */
-import { clerkClient, WebhookEvent } from "@clerk/nextjs/server";
+import { clerkClient } from "@clerk/nextjs/server";
+import { WebhookEvent } from "@clerk/nextjs/server";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 import { Webhook } from "svix";
+
 import { createUser, deleteUser, updateUser } from "@/lib/actions/user.actions";
 
 export async function POST(req: Request) {
+  // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
   const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
 
   if (!WEBHOOK_SECRET) {
@@ -14,23 +17,29 @@ export async function POST(req: Request) {
     );
   }
 
+  // Get the headers
   const headerPayload = headers();
   const svix_id = headerPayload.get("svix-id");
   const svix_timestamp = headerPayload.get("svix-timestamp");
   const svix_signature = headerPayload.get("svix-signature");
 
+  // If there are no headers, error out
   if (!svix_id || !svix_timestamp || !svix_signature) {
     return new Response("Error occured -- no svix headers", {
       status: 400,
     });
   }
 
+  // Get the body
   const payload = await req.json();
   const body = JSON.stringify(payload);
+
+  // Create a new Svix instance with your secret.
   const wh = new Webhook(WEBHOOK_SECRET);
 
   let evt: WebhookEvent;
 
+  // Verify the payload with the headers
   try {
     evt = wh.verify(body, {
       "svix-id": svix_id,
@@ -44,30 +53,27 @@ export async function POST(req: Request) {
     });
   }
 
+  // Get the ID and type
   const { id } = evt.data;
   const eventType = evt.type;
 
   // CREATE
   if (eventType === "user.created") {
-    const { email_addresses, image_url, first_name, last_name, username } =
+    const { id, email_addresses, image_url, first_name, last_name, username } =
       evt.data;
 
-    // Ensure that clerkId is not undefined
-    if (!id) {
-      return new Response("Missing Clerk user ID", { status: 400 });
-    }
-
     const user = {
-      clerkId: id, // Ensure clerkId is always a string
+      clerkId: id,
       email: email_addresses[0].email_address,
-      username: username ?? "", // Fallback to empty string if null
-      firstName: first_name ?? "", // Fallback to empty string if null
-      lastName: last_name ?? "", // Fallback to empty string if null
-      photo: image_url ?? "", // Ensure it's never null
+      username: username!,
+      firstName: first_name || "",
+      lastName: last_name || "",
+      photo: image_url,
     };
 
     const newUser = await createUser(user);
 
+    // Set public metadata
     if (newUser) {
       await clerkClient.users.updateUserMetadata(id, {
         publicMetadata: {
@@ -81,17 +87,14 @@ export async function POST(req: Request) {
 
   // UPDATE
   if (eventType === "user.updated") {
-    const { image_url, first_name, last_name, username } = evt.data;
+    const { id, image_url, first_name, last_name, username } = evt.data;
 
     const user = {
-      firstName: first_name ?? "",
-      lastName: last_name ?? "",
-      username: username ?? "",
-      photo: image_url ?? "",
+      firstName: first_name || "",
+      lastName: last_name || "",
+      username: username!,
+      photo: image_url,
     };
-    if (!id) {
-      return new Response("Missing Clerk user ID", { status: 400 });
-    }
 
     const updatedUser = await updateUser(id, user);
 
@@ -100,15 +103,14 @@ export async function POST(req: Request) {
 
   // DELETE
   if (eventType === "user.deleted") {
-    if (!id) {
-      return new Response("Missing Clerk user ID", { status: 400 });
-    }
-    const deletedUser = await deleteUser(id);
+    const { id } = evt.data;
+
+    const deletedUser = await deleteUser(id!);
 
     return NextResponse.json({ message: "OK", user: deletedUser });
   }
 
-  console.log(`Webhook with an ID of ${id} and type of ${eventType}`);
+  console.log(`Webhook with and ID of ${id} and type of ${eventType}`);
   console.log("Webhook body:", body);
 
   return new Response("", { status: 200 });
